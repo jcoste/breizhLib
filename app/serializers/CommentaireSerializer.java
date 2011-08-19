b2b7db99e9538ef534109fa2ef40f7a432807591
gsylvain35 <gsylvain35@gmail.com>
2011-08-19 23:06:31 +0200

    version 0-1-6

--- app/serializers/CommentaireSerializer.java
+++ app/serializers/CommentaireSerializer.java
@@ -16,14 +16,11 @@ public class CommentaireSerializer implements JsonSerializer<Commentaire> {
     public JsonElement serialize(Commentaire commentaire, Type type, JsonSerializationContext jsonSerializationContext) {
         JsonObject obj = new JsonObject();
         obj.addProperty("avis", commentaire.commentaire);
-        obj.addProperty("nom", commentaire.nom+" le "+new SimpleDateFormat("dd MMMM yyyy").format(commentaire.dateAjout));
+        obj.addProperty("nom", commentaire.nom);
+        obj.addProperty("titre", commentaire.nom+" le "+new SimpleDateFormat("dd MMMM yyyy").format(commentaire.dateAjout));
         obj.addProperty("note", commentaire.note);
         obj.addProperty("uid", commentaire.getUid());
         obj.add("livre",jsonSerializationContext.serialize(commentaire.livre));
-
-        // pour etre compatible avec les versions 0.1.0 et 0.1.1 de l'application android
-        obj.addProperty("isbn", commentaire.livre.iSBN);
-        obj.addProperty("titre", commentaire.livre.titre);
         return obj;
     }
 }
