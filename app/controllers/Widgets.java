d056e03b554eff35e83d123b8d6ddce8be650b6d
gsylvain35 <gsylvain35@gmail.com>
2011-10-12 22:31:02 +0200

    gestion widgets

--- app/controllers/Widgets.java
+++ app/controllers/Widgets.java
@@ -5,8 +5,11 @@ import controllers.security.Secure;
 import models.Commentaire;
 import models.Widget;
 import models.WidgetData;
+import models.socialoauth.Role;
 import models.tag.LivreTag;
 import models.tag.Tag;
+import play.data.validation.Required;
+import play.modules.router.Post;
 import play.mvc.Before;
 import play.mvc.Controller;
 import play.mvc.With;
@@ -49,4 +52,41 @@ public class Widgets extends Controller {
 
         renderArgs.put("widget", data);
     }
+
+    @Role("admin")
+    public static void admin(){
+        List<String> roles = new ArrayList<String>();
+        roles.add("public");
+        roles.add("member");
+        roles.add("admin");
+
+        List<Widget> widgets =Widget.findAll();
+        render(roles,widgets);
+    }
+
+     @Role("admin")
+    public static void delete(Long id){
+       Widget widget = Widget.findById(id);
+       if(widget != null){
+           widget.delete();
+       }
+       admin();
+    }
+
+    @Post("/Widgets/add")
+    public static void save(@Required String titre,@Required String template, @Required String role){
+         if (validation.hasErrors()) {
+            render("Widgets/add.html");
+        }
+
+        Widget widget = Widget.findByTitre(titre);
+
+        if(widget == null){
+            widget = new Widget(titre,template,role);
+            widget.insert();
+        }
+        admin();
+    }
 }
+
+
